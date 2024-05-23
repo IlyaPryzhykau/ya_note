a85a5958ec695014917393492d74cdae7626a679
Ilya_Pryzhykau <tuarone@gmail.com>
2024-05-23 20:56:59 +0200

    third

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 from notes.models import Note
+from notes.forms import NoteForm
 
 User = get_user_model()
 
@@ -22,7 +23,9 @@ class TestNotesPage(TestCase):
         """
 
         cls.author = User.objects.create(username='Лев Толстой')
-        all_notes = [
+        cls.other_user = User.objects.create(username='Саня Пушкин')
+
+        cls.notes = [
             Note(
                 author=cls.author,
                 title=f'Запись {index}',
@@ -31,7 +34,63 @@ class TestNotesPage(TestCase):
             )
             for index in range(cls.NOTES_COUNT_ON_NOTES_LIST)
         ]
-        Note.objects.bulk_create(all_notes)
+        Note.objects.bulk_create(cls.notes)
+
+        cls.notes = list(Note.objects.filter(author=cls.author))
+        cls.other_note = Note.objects.create(
+            author=cls.other_user,
+            title='Чужая заметка',
+            text='Текст чужой заметки',
+            slug='chuzhaya-zametka'
+        )
+
+    def test_note_in_object_list(self):
+        """
+        Тест: Отдельная заметка передаётся на страницу со списком
+        заметок в списке object_list, в словаре context.
+        """
+
+        self.client.force_login(self.author)
+        response = self.client.get(self.NOTES_LIST)
+        object_list = response.context['object_list']
+
+        self.assertIn(self.notes[0], object_list)
+
+    def test_other_user_notes_not_in_list(self):
+        """
+        Тест: В список заметок одного пользователя
+        не попадают заметки другого пользователя.
+        """
+
+        self.client.force_login(self.author)
+        response = self.client.get(self.NOTES_LIST)
+        object_list = response.context['object_list']
+
+        self.assertNotIn(self.other_note, object_list)
+
+    def test_note_add_page_contains_form(self):
+        """
+        Тест: На страницу создания заметки передаётся форма.
+        """
+
+        self.client.force_login(self.author)
+        add_url = reverse('notes:add')
+        response = self.client.get(add_url)
+
+        self.assertIn('form', response.context)
+        self.assertIsInstance(response.context['form'], NoteForm)
+
+    def test_note_edit_page_contains_form(self):
+        """
+        Тест: На страницу редактирования заметки передаётся форма.
+        """
+
+        self.client.force_login(self.author)
+        edit_url = reverse('notes:edit', args=(self.notes[0].slug,))
+        response = self.client.get(edit_url)
+
+        self.assertIn('form', response.context)
+        self.assertIsInstance(response.context['form'], NoteForm)
 
     def test_notes_count(self):
         """
